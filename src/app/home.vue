5b84fc0b29b010c5ff3dba6fe236debee21ddd11
Shan Dhiviyarajan <shandhiviyarajan@CMSs-Mac-mini.local>
2018-12-21 19:04:51 +0530

    initial commit

---
+++ src/app/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <f7-page>
+        <f7-navbar title="Statusbar Overlay">
+            <img src="../images/svg/menu.svg" alt="">
+        </f7-navbar>
+    </f7-page>
+</template>
+<script>
+    import { f7Navbar, f7Page, f7Block, f7Row, f7Button } from 'framework7-vue';
+
+    export default {
+        components: {
+            f7Navbar,
+            f7Page,
+            f7Block,
+            f7Row,
+            f7Button,
+        }
+    };
+</script>
